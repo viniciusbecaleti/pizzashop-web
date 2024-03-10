0bb9c7811dd570d52101537d21d577b0e9dfd154
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-03-10 13:43:20 -0300

    feat: adiciona estilização da página de SignIn

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,10 +1,21 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Auth Layout</h1>
-      <Outlet />
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span>Pizza Shop</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; Pizza Shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <Outlet />
+      </div>
     </div>
   )
 }
