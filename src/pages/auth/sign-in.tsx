f0f59f7deadd92f99be9850baeed6d78b50cae5d
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-03-10 14:14:27 -0300

    feat: adiciona sonner para notifiações toast

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -23,6 +24,8 @@ export function SignIn() {
     await new Promise((resolve) => setTimeout(resolve, 2000))
 
     console.log(data)
+
+    toast.success(`Enviamos um link de autenticação para o email`)
   }
 
   return (
