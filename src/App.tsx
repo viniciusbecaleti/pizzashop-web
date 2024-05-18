251dfe29c16312d974235711814e98256c37010d
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-05-18 13:13:39 -0300

    feat: adiciona tailwind e shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
-    <>
-      <main>Hello, world!</main>
-    </>
+    <div>
+      <h1>Hello, world!</h1>
+      <Button>Click me</Button>
+    </div>
   );
 }
