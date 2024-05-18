8715c8daa0a900769ba9419d265f134a90ca95d7
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-05-18 18:46:29 -0300

    feat: refaz formulário usando o Form do shadcn/ui e adiciona react-hook-form e zod para tratar o submit do formulário e suas devidas validações

--- src/pages/auth/sign-in/index.tsx
+++ src/pages/auth/sign-in/index.tsx
@@ -1,10 +1,43 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+
+const signInFormSchema = z.object({
+  email: z.string().email({
+    message: 'E-mail inválido',
+  }),
+})
+
+type signInFormType = z.infer<typeof signInFormSchema>
 
 export function SignInPage() {
+  const form = useForm<signInFormType>({
+    resolver: zodResolver(signInFormSchema),
+    defaultValues: {
+      email: '',
+    },
+  })
+
+  const { handleSubmit, formState } = form
+
+  async function handleSignIn(data: signInFormType) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    console.log(data)
+  }
+
   return (
     <>
       <Helmet title="Entrar" />
@@ -18,16 +51,30 @@ export function SignInPage() {
               Acompanhe suas vendas pelo painel do parceiro
             </p>
           </div>
-          <form className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
-            </div>
-
-            <Button type="submit" className="w-full">
-              Acessar painel
-            </Button>
-          </form>
+          <Form {...form}>
+            <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="space-y-2">
+                    <FormLabel>Seu e-mail</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={formState.isSubmitting}
+              >
+                Acessar painel
+              </Button>
+            </form>
+          </Form>
         </div>
       </main>
     </>
