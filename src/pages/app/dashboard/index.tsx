7760dfc19df78672e0c7584ac2d9e19a3097afb6
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-05-19 13:38:07 -0300

    feat: adiciona componentes de receita, pedidos, e cancelamentos da dashboard

--- src/pages/app/dashboard/index.tsx
+++ src/pages/app/dashboard/index.tsx
@@ -1,10 +1,24 @@
 import { Helmet } from 'react-helmet-async'
 
+import { DayOrdersAmountCard } from './_components/day-orders-amount-card'
+import { MonthCanceledOrdersAmountCard } from './_components/month-canceled-orders-amount-card'
+import { MonthOrdersAmountCard } from './_components/month-orders-amount-card'
+import { MonthRevenueCard } from './_components/month-revenue-card'
+
 export function DashboardPage() {
   return (
     <main>
       <Helmet title="Dashboard" />
-      <h1>Dashboard Page</h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
+        </div>
+      </div>
     </main>
   )
 }
